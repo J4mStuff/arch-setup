53c5451ed69b0423c9a1cb13430758e20497aaa9
J4mStuff <59264088+J4mStuff@users.noreply.github.com>
2025-07-26 20:29:22 +0200

    add flatpaks

--- get_installed_packages.sh
+++ get_installed_packages.sh
@@ -2,15 +2,12 @@
 
 paru='pkgs/paru.txt'
 pacman='pkgs/pacman.txt'
+flatpak='pkgs/flatpak.txt'
 
-#paru_pkgs=($(sudo paru -Qem | awk '{print $1}'))
 mapfile -t paru_pkgs < <(sudo paru -Qem | awk '{print $1}')
 mapfile -t pacman_pkgs < <(sudo pacman -Qen | awk '{print $1}')
+mapfile -t flatpak_pkgs < <(flatpak list --app --columns=application)
 
-printf '%s\n' "${paru_pkgs[@]}"
 printf '%s\n' "${paru_pkgs[@]}" > $paru
-
-printf "\n\n\n"
-
-printf '%s\n' "${pacman_pkgs[@]}"
 printf '%s\n' "${pacman_pkgs[@]}" > $pacman
+printf '%s\n' "${flatpak_pkgs[@]}" > $flatpak
